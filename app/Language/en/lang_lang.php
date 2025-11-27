408716dc86adb167890c25d8b148cc201a6d289d
tejaspilke <tejaspilke1@gmail.com>
2025-11-27 10:58:31 +0000

    lang file update

--- app/Language/en/lang_lang.php
+++ app/Language/en/lang_lang.php
@@ -6,7 +6,7 @@
  */
 
 $lang['list'] = 'List';
-<<<<<<< HEAD
+
 $lang['registration'] = 'Registration';
 $lang['course'] = 'Course';
 $lang['year'] = 'Year';
@@ -14,9 +14,11 @@ $lang['academic year'] = 'Academic Year';
 $lang['email'] = 'Email';
 $lang['admission'] = 'Admission';
 $lang['add'] = 'Add';
-=======
+
 $lang['ticket'] = 'Ticket';
 $lang['rise'] = 'Rise';
 $lang['add'] = 'Add';
 $lang['registration'] = 'Registration';
->>>>>>> 41350f2cc243651de66f82af82835eccade37eac
+
+$lang['update']='Update';
+$lang['delete']='Delete';
