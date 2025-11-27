9b28b91bd61fbebc9ed6e2454fef35285ed05d60
vikram <vikramgawadeofficial@gmail.com>
2025-11-27 11:28:40 +0000

    new lang upadted by vikram

--- app/Language/en/lang_lang.php
+++ app/Language/en/lang_lang.php
@@ -1,30 +1,4 @@
 <?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
-$lang['list'] = 'List';
-
-$lang['registration'] = 'Registration';
-$lang['course'] = 'Course';
-$lang['year'] = 'Year';
-$lang['academic year'] = 'Academic Year';
-$lang['email'] = 'Email';
-$lang['admission'] = 'Admission';
-$lang['add'] = 'Add';
-
-$lang['ticket'] = 'Ticket';
-$lang['rise'] = 'Rise';
-$lang['registration'] = 'Registration';
-
-$lang['update']='Update';
-$lang['delete']='Delete';
-$lang['feedback'] = 'Feedback';
-$lang['master'] = 'Master';
-$lang['create'] = 'Create';
-$lang['contact'] = 'Contact';
-$lang['leaving'] = 'Leaving';
-$lang['certificate'] = 'Certificate';
-
+//d
+$lang['distance'] = 'Distance';
+$lang['develop'] = 'Develop';
