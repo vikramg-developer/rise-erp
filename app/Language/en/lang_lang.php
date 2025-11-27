272ce820d9c4ab8b08b5996ecbbdfd9339bcc6a6
jadhavsonal590-sj <jadhavsonal590@gmail.com>
2025-11-27 10:58:24 +0000

    language file update

--- app/Language/en/lang_lang.php
+++ app/Language/en/lang_lang.php
@@ -6,7 +6,6 @@
  */
 
 $lang['list'] = 'List';
-<<<<<<< HEAD
 $lang['registration'] = 'Registration';
 $lang['course'] = 'Course';
 $lang['year'] = 'Year';
@@ -14,9 +13,9 @@ $lang['academic year'] = 'Academic Year';
 $lang['email'] = 'Email';
 $lang['admission'] = 'Admission';
 $lang['add'] = 'Add';
-=======
 $lang['ticket'] = 'Ticket';
 $lang['rise'] = 'Rise';
 $lang['add'] = 'Add';
 $lang['registration'] = 'Registration';
->>>>>>> 41350f2cc243651de66f82af82835eccade37eac
+$lang['create'] = 'Create';
+$lang['contact'] = 'Contact';
