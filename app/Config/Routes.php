aad8b66b5f562aefa2e158d3ce3c1fd31309cd9f
sdc-erp <sdc@kbpcoes.edu.in>
2025-12-03 11:36:45 +0530

    Update Routes.php
    routes Home changed to home

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->add('/', 'Home::index');
-$routes->add('/Home', 'Home::index');
+$routes->add('/home', 'Home::index');
 $routes->add('/leaving-certificate', 'LeavingCertificate::index');
 $routes->add('/lc-remark', 'LeavingCertificate::addLeavingCertificateRemark');
 
