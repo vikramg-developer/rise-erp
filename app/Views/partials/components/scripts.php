0365a9f68d4ce6fda280a0a89019bfa34d8d8f7b
Shoeb Kazi <shoeb.kazi23@gmail.com>
2025-12-06 13:12:55 +0000

    script file updated

--- app/Views/partials/components/scripts.php
+++ app/Views/partials/components/scripts.php
@@ -50,7 +50,7 @@
 <script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
 
 <!-- Datatables Cdn -->
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.jgit statuss"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
