722812219353e560e3bb52dae0529cb9aa71a6a8
vikram <vikramgawadeofficial@gmail.com>
2025-12-13 11:15:52 +0000

    course,academic_year, Year Automodels updated code

--- app/Models/ModelLeavingCertificate.php
+++ app/Models/ModelLeavingCertificate.php
@@ -1,38 +1,34 @@
 <?php
+
 namespace App\Models;
 
 use CodeIgniter\Model;
 
-class ModelLeavingCertificate extends Model
-{
-    protected $table            = 'leaving_certificate';
-    protected $primaryKey       = 'lc_id';
+class ModelLeavingCertificate extends Model {
+
+    protected $table = 'leaving_certificate';
+    protected $primaryKey = 'lc_id';
     protected $useAutoIncrement = true;
-
-    protected $returnType       = 'array'; // You can change to 'object' if needed
-    protected $useSoftDeletes   = false;   // You are using is_deleted instead of softDelete()
-
+    protected $returnType = 'array'; // You can change to 'object' if needed
+    protected $useSoftDeletes = false;   // You are using is_deleted instead of softDelete()
     protected $allowedFields = [
         'ysd_id',
         'examination',
         'exam_held_in',
         'date_of_leaving',
         'is_duplicate',
-        'previous_lc_date',        
+        'previous_lc_date',
         'is_deleted'
     ];
-
     // Validation Rules (Optional - add later if required)
-    protected $validationRules = [        
+    protected $validationRules = [
         'course_id' => 'required',
         'year_id' => 'required',
         'aca_year_id' => 'required'
     ];
     protected $validationMessages = [
-        'course_id'=>['required'=>'course is required'],
-        'year_id'=>['required'=>'course is required'],
-        'aca_year_id'=>['required'=>'course is required'],
-        
+        'course_id' => ['required' => 'course is required'],
+        'year_id' => ['required' => 'course is required'],
+        'aca_year_id' => ['required' => 'course is required'],
     ];
-//    protected $skipValidation = false;
 }
